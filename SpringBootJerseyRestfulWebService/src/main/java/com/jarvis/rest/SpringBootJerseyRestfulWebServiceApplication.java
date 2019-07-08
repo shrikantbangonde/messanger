e296ad9a0ef50bdee3a2ba06fcc95d99c286bbd2
shrikantbangonde <bangondeshrikant@gmail.com>
2019-07-08 11:21:01 +0530

    second commit

---
+++ SpringBootJerseyRestfulWebService/src/main/java/com/jarvis/rest/SpringBootJerseyRestfulWebServiceApplication.java
@@ -0,0 +1,13 @@
+package com.jarvis.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJerseyRestfulWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJerseyRestfulWebServiceApplication.class, args);
+	}
+
+}
